296c51d0838c019db94b38253b60bbc01b850abf
Smarty Indonesia <smartyindo@gmail.com>
2026-03-18 09:56:54 +0700

    update

--- api/index.php
+++ api/index.php
@@ -2,6 +2,26 @@
 $pagetitle="Home";
 include "includes/head.php";
 ?>
+<style>
+  .word-wrap {
+  /* These are technically the same, but use both */
+  overflow-wrap: break-word;
+  word-wrap: break-word;
+
+  -ms-word-break: break-all;
+  /* This is the dangerous one in WebKit, as it breaks things wherever */
+  word-break: break-all;
+  /* Instead use this non-standard one: */
+  word-break: break-word;
+
+  /* Adds a hyphen where the word breaks, if supported (No Blink) */
+  -ms-hyphens: auto;
+  -moz-hyphens: auto;
+  -webkit-hyphens: auto;
+  hyphens: auto;
+}
+</style>
+
   <div class="hero_area">
     <!-- header section strats -->
     <?php include "includes/navbar.php" ?>
